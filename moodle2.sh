21a9f67f924c981e09a606f0047cfddb4f7c3c70
Shahadan Bin Saad <shahadan@melaka.uitm.edu.my>
2019-03-15 09:49:40 +0800

    modified:   moodle2.sh 	modified:   test.txt

--- moodle2.sh
+++ moodle2.sh
@@ -9,3 +9,17 @@ t="mdl_assignment_upgrade"
 echo "t =" $t
 mysql -uroot -ptoor -e "select oldinstance, newinstance from $t" $d >> $filename 
 
+t="mdl_block_instances"
+echo "t =" $t
+mysql -uroot -ptoor -e "select id, blockname, parentcontextid, showinsubcontexts, requiredbytheme, pagetypepattern, subpagepattern, defaultregion, defaultweight, configdata from $t" $d >> $filename
+
+t="mdl_block_positions"
+echo "t =" $t
+mysql -uroot -ptoor -e "select blockinstanceid  from $t" $d >> $filename
+
+t="mdl_context"
+echo "t =" $t
+mysql -uroot -ptoor -e "select instanceid  from $t" $d >> $filename
+
+
+
